ff779be59d78fd6de22d55f524eb6b2184209d31
Yash Dhokane <146187969+yashh1212@users.noreply.github.com>
2025-01-13 23:41:10 +0530

    Update bio section

--- src/component/About.js
+++ src/component/About.js
@@ -8,7 +8,7 @@ function About() {
       </div>
 
       <div className="personal_details">
-        <p>
+        {/* <p>
           Hello, I'm Yash, a passionate full-stack developer with a diploma in
           Computer Engineering and an <span>8.8 CGPA </span>score. Skilled in
           front-end and back-end development, I create efficient web
@@ -19,14 +19,21 @@ function About() {
           . Experienced in <span>Git, databases, and cloud deployment</span>, I
           excel in solving complex problems and adapting to new technologies. I
           collaborate effectively to deliver high-quality projects that exceed
-          client expectations..
-        </p>
-
+          client expectations.
+        </p> */}
         <p>
-          This version maintains the key details while reducing redundancy and
-          making the text more streamlined and easier to read. Adjustments were
-          made to focus on essential skills and achievements, ensuring clarity
-          and impact.
+          Hello, I'm Yash, a passionate full-stack developer currently pursuing
+          a B.Tech in Cybersecurity Engineering (2nd year) with an{" "}
+          <span>8.29 CGPA</span>. I hold a diploma in Computer Engineering,
+          where I achieved an <span>8.8 CGPA</span>. Skilled in front-end and
+          back-end development, I create efficient web applications using
+          <span>
+            HTML,CSS, JavaScript, React, Node.js, Python, Express, and MongoDB.{" "}
+          </span>
+          Experienced in <span>Git, databases, and cloud deployment</span>, I
+          excel at solving complex problems, adapting to new technologies, and
+          collaborating effectively to deliver high-quality projects that exceed
+          expectations.
         </p>
       </div>
     </div>
